8e5204e896f41d60b4f323fc68f5f47885a5e991
Tharanidharan M <155654877+tharanidharanm23@users.noreply.github.com>
2026-05-27 21:07:47 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+
+1. Hello World Program
+• Objective: Understand the basic structure of a Java program.
+• Task: Write a Java program that prints "Hello, World!" to the console.
+• Instructions:
+o Create a class named HelloWorld.
+o Implement the main method.
+o Use System.out.println() to display the message
+
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+}
